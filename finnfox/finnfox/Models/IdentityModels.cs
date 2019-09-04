d22c8ae065a6f0c17ac2b11bec5262e2fe41591f
Milos <dobrota.milos@yahoo.com>
2019-09-04 21:46:28 +0200

    AddedMigration for UserLastName

--- finnfox/finnfox/Models/IdentityModels.cs
+++ finnfox/finnfox/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace finnfox.Models
             // Add custom user claims here
 
             userIdentity.AddClaim(new Claim("UserLastName" ,this.UserLastName.ToString() ) );
-
+            
             return userIdentity;
         }
     }
