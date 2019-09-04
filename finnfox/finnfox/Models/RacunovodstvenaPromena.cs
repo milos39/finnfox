9fd40d9ed2e11233cfb3ee76ce2ff4cdacd53f3f
Aleksa <aleksa.kuzman.996@gmail.com>
2019-09-04 19:25:46 +0200

    Identity i Initial migration
    Dodat identity sa inicijalnom migracijom koja sadrzi i dve tabele (RacunovodstvenaPromena i TipRacunovodstvenePromene)

---
+++ finnfox/finnfox/Models/RacunovodstvenaPromena.cs
@@ -0,0 +1,26 @@
+namespace finnfox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("RacunovodstvenaPromena")]
+    public partial class RacunovodstvenaPromena
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int PromenaId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NazivPromene { get; set; }
+
+        public DateTime DatumPromene { get; set; }
+
+        public int TipPromeneId { get; set; }
+
+        public virtual TipRacunovodstvenePromene TipRacunovodstvenePromene { get; set; }
+    }
+}
---
+++ finnfox/finnfox/Models/TipRacunovodstvenePromene.cs
@@ -0,0 +1,31 @@
+namespace finnfox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TipRacunovodstvenePromene")]
+    public partial class TipRacunovodstvenePromene
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TipRacunovodstvenePromene()
+        {
+            RacunovodstvenaPromenas = new HashSet<RacunovodstvenaPromena>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int TipPromeneId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NazivTipa { get; set; }
+
+        public bool PozitivnostTipa { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<RacunovodstvenaPromena> RacunovodstvenaPromenas { get; set; }
+    }
+}
