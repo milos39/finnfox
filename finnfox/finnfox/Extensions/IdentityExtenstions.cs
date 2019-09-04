8c2f392a1a728455053853230bda0574ebff6948
Milos <dobrota.milos@yahoo.com>
2019-09-04 21:41:18 +0200

    LastName added to aspNetUsers
    With extension method and user claim

---
+++ finnfox/finnfox/Extensions/IdentityExtenstions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace finnfox.Extensions
+{
+    public static class IdentityExtenstions
+    {
+        public static string GetUserLastName(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("UserLastName");
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
