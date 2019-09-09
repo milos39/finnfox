c7283037b9a4c116b922d6c90a683f9fceb85c7e
milos39 <dobrota.milos@yahoo.com>
2019-09-09 16:10:16 +0200

    implementiran angularjs, chart.js, bootstrap table , bootstrapDatepicker

---
+++ finnfox/finnfox/Scripts/NGracunovodstvenePromene.js
@@ -0,0 +1,41 @@
+﻿var app = angular.module("racunovodstvenePromene", []);
+
+app.controller('racunovodstvenePromeneController', function ($scope, $http) {
+    function loadPromene(godina) {
+        godina = godina || 0;
+
+        $http.get("http://localhost:1091/RacunovodstvenaPromenas/promenePoGodini?godina=" + godina).then(function (result) {
+            console.log(result);
+            var tabela = $("#promeneTabela");
+            $scope.promenePoGodini = result.data;
+            $("#promeneTabela").bootstrapTable({ data: result.data.racunovodstvenePromene });
+            tabela.bootstrapTable('load', result.data.racunovodstvenePromene);
+
+            if (godina !== 0) {
+                $("#godina-naslov").html("Finansije u " + godina);
+            } else {
+                $("#godina-naslov").html("Kompletna finansijska istorija");
+            }
+        });
+        
+    }
+    loadPromene();
+    //$("#godine-dropdown").click(function (event) {
+    $(document).on('click', '#godine-dropdown li a', function () {
+        event.preventDefault();
+        event.stopPropagation();
+        var kliknutaGodina = $(this).attr("class");
+        if (kliknutaGodina.length == 4 || kliknutaGodina == 0) {
+            console.log("uspeh jer je kliknut: " + kliknutaGodina + ' !');
+            loadPromene(kliknutaGodina);
+        } else {
+            console.log("neuspeh jer je kliknut: " + kliknutaGodina);
+            console.log(kliknutaGodina.length);
+            return;
+        }
+        
+
+        console.log("clicked " + kliknutaGodina);
+        
+    });
+});
