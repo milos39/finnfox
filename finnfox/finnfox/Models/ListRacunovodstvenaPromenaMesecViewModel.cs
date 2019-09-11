68a5ad93c6a0404ca21e312bfa4d683233bf8a97
Milos <dobrota.milos@yahoo.com>
2019-09-11 22:15:28 +0200

    promenePoMesecu dodate

---
+++ finnfox/finnfox/Models/ListRacunovodstvenaPromenaMesecViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace finnfox.Models
+{
+    public class ListRacunovodstvenaPromenaMesecViewModel
+    {
+
+        public IList<RacunovodstvenaPromenaDTO> racunovodstvenePromene { get; set; }
+        public  double balans { get; set; }
+        public IList<int> godine { get; set; }
+        public IList<int> meseciZaDatuGodinu { get; set; }
+
+
+        public ListRacunovodstvenaPromenaMesecViewModel()
+        {
+            racunovodstvenePromene = new List<RacunovodstvenaPromenaDTO>();
+            godine = new List<int>();
+            meseciZaDatuGodinu = new List<int>();
+        }   
+
+    }
+
+}
